3753b99e569fd08d5c132381028be7c3c593eb1b
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-02-10 19:23:02 +0800

    Add files via upload

--- pages/dashboard/add_question.php
+++ pages/dashboard/add_question.php
@@ -1,6 +1,8 @@
 <?php
+session_start();
 	include'../config.php';
-	
+	if(isset($_POST['submit'])){
+
 	$examId=$_POST['examId'];
 	$type =$_POST['type'];
 	$question=$_POST['question'];
@@ -8,11 +10,34 @@
 	$choice_B=$_POST['choice_B'];
 	$choice_C=$_POST['choice_C'];
 	$choice_D=$_POST['choice_D'];
-	$correctAnswer=$_POST['correctAnswer'];
-	
-	mysqli_query($conn,"insert into exam_question (exam_id, exam_question, exam_ch1, exam_ch2, exam_ch3, exam_ch4, exam_answer, q_type)
+	$correctAnswer=$_POST['correctAnswer']; 
+
+	if($type == 'Multiple Choice'){
+		if($correctAnswer != $choice_A AND $correctAnswer != $choice_B AND $correctAnswer != $choice_C AND $correctAnswer != $choice_D){
+			mysqli_query($conn,"insert into exam_question (exam_id, exam_question, exam_ch1, exam_ch2, exam_ch3, exam_ch4, exam_answer, q_type)
+			values ('$examId', '$question', '$choice_A','$choice_B', '$choice_C','$choice_D', '$correctAnswer', '$type')");
+			$_SESSION['message'] = "Please input correct answer";
+			header("location:manage-exam.php?id=$examId");
+			exit(0);
+		}else{
+
+			mysqli_query($conn,"insert into exam_question (exam_id, exam_question, exam_ch1, exam_ch2, exam_ch3, exam_ch4, exam_answer, q_type)
+			values ('$examId', '$question', '$choice_A','$choice_B', '$choice_C','$choice_D', '$correctAnswer', '$type')");
+		   header("location:manage-exam.php?id=$examId");
+
+		}
+		
+
+	}else{
+		mysqli_query($conn,"insert into exam_question (exam_id, exam_question, exam_ch1, exam_ch2, exam_ch3, exam_ch4, exam_answer, q_type)
 	 values ('$examId', '$question', '$choice_A','$choice_B', '$choice_C','$choice_D', '$correctAnswer', '$type')");
 	header("location:manage-exam.php?id=$examId");
+	}
+	
+	
+}
+	
+	
 
 ?>
 
