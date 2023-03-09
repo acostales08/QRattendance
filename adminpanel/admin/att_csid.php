a08cc7ee2f18afe55067b6059f01f555795be266
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-03-09 19:43:32 +0800

    Delete adminpanel directory

--- adminpanel/admin/att_csid.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../config.php';
-$class_subject = $_POST['class'];
-header("location: admin.php?id=$class_subject");
-exit(0);
-
-
-  
-?>
--- adminpanel/class_subject/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-include '../../pages/config.php';
-	$id=$_GET['id'];
-	mysqli_query($conn,"delete from class_subject where id='$id'");
-	header('location:class_subject.php');
-	exit(0);
-?>
