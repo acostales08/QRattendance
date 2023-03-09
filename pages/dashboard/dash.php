b55632ac76673cfd1f947b4ddc7fa6d82a26b0b9
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-03-09 19:37:19 +0800

    Delete pages directory

--- pages/dashboard/dash.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../config.php';
-$class_subject = $_POST['class'];
-header("location: dashboard.php?id=$class_subject");
-exit(0);
-
-
-  
-?>
--- pages/dashboard/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('../config.php');
-	$id=$_GET['id'];
-                	
-	mysqli_query($conn,"delete from exam where ex_id='$id'");
-	header('location:exam.php');
-	exit(0);
-
-?>
