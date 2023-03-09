641be898abcb2b565942cf587acfca00a5a44ba0
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-03-09 20:01:59 +0800

    Delete pages directory

--- pages/dashboard/class_csid.php
+++
@@ -1,5 +0,0 @@
-<?php
-     $class_subject = $_POST['class'];
-     header("location: class.php?id=$class_subject");
-    exit(0);
-?>
--- pages/footer.php
+++
@@ -1,3 +0,0 @@
-<footer class="main-footer">
-    <strong>Copyright &copy; 2022-2023</strong> All rights reserved.
-  </footer>
