e88b837a03cf9006444dd0c82eee0a0435b5bbfa
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-03-10 17:25:40 +0800

    Add files via upload

---
+++ adminpanel/admin/rec_pass_code.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include '../../pages/config.php';
+
+
+if(isset($_POST['submit_btn'])){
+    
+    $token = $_POST['token'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $confirmpass = $_POST['confirmpassword'];
+
+
+        if($password == $confirmpass){
+            $pass = md5($password);
+
+            $query = mysqli_query($conn, "UPDATE users set password='$pass' where email='$email'");
+            ob_end_flush();
+            header('location:index.php');
+            exit(0);
+        }else{
+            $_SESSION['message'] = "Passwords do not match";
+            header("location:recover_password.php?token=$token");
+            ob_end_flush();
+            exit(0);
+        }
+    
+}
+
+?>
---
+++ adminpanel/sy_sem/add_new.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include '../../pages/config.php';
+	
+	$course=$_POST['course_id'];
+	$level=$_POST['level'];
+	$section=$_POST['section'];
+
+	$query = mysqli_query($conn, "SELECT * FROM class where course_id = '$course' AND level  = '$level' AND section  = '$section'" );
+
+	if (mysqli_num_rows($query) > 0) {
+		$_SESSION['message'] = "Class is already exist";
+		header('location:class.php');
+			exit(0);
+
+	}else{
+	
+	mysqli_query($conn,"insert into class (course_id, level, section) values ('$course','$level', '$section')");
+	header('location:class.php');
+	exit(0);
+	}
+
+?>
