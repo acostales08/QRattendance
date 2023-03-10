5721b10d6f09b9606f5d54c75a98843421d98b51
acostales08 <79326346+acostales08@users.noreply.github.com>
2023-03-10 17:19:20 +0800

    Delete adminpanel directory

--- adminpanel/student/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-include '../../pages/config.php';
-	$id=$_GET['id'];
-	mysqli_query($conn,"delete from student_info where sid='$id'");
-	header('location:student.php');
-	ob_end_flush();
-	exit(0);
-
-?>
--- adminpanel/subject/delete.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include'../../pages/config.php';
-	$id=$_GET['id'];
-	mysqli_query($conn,"delete from subjects where id='$id'");
-	header('location:subject.php');
-	ob_end_flush();
-	exit(0);
-
-?>
